c0ce090f5c7fc08121163ab5cecaabe473f2675f
Jim Denis <jimdenis007@yahoo.com>
2020-03-16 16:51:11 -0700

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # burgers_handlebars
-Burgers app using Node Express Handlebars 
+
+Burgers app using Node Express Handlebars -- home work # 12
+
+Jim Denis --- Home Work 13 --- Date 03/16/20 
+
+Created a program to allows users to update several MySQL tables about employees
+
+you should be able to see a viedo of me using the app at...  
+
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Old Fashion');
+INSERT INTO burgers (name) VALUES ('Double Patties');
+INSERT INTO burgers (name, eaten) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Meatless', true);
+INSERT INTO burgers (name) VALUES ('One Pounder');
